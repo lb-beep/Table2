9adc798641cad7da1ecee8b022672394a0a5bb4b
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-01-16 08:25:31 -0700

    Add files via upload

---
+++ Tables/Tables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tables
+{
+    class Program
+    {
+        static void myDivision()
+        {
+            int j = 600;
+            int b = 6;
+            int a = 0;
+
+            a = j / b;
+            Console.WriteLine(a);
+        }
+        static void Main(string[] args)
+        {
+            myDivision();
+
+
+        }
+    }
+}
